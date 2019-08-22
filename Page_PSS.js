c4261245fbe3bc2f9729d2d30e1748768b139138
VaclavSoupa <36476635+VaclavSoupa@users.noreply.github.com>
2019-08-22 14:37:45 +0200

    11

--- Page_PSS.js
+++
@@ -1,9 +0,0 @@
-class Page_PSS {
-	
-  	//Adds QM to the page context due to the fact
-  	//that creating QM could be quite costly in case of PSS execution
-    public static function AddQMToPageContext(confirmit, report, user, state, pageContext){
-      var qm = QueryManagerBreakByInterface.GetQM('PSS', confirmit, report, user, state);
-      pageContext.Items[PageContextEnum.qm] = qm;
-  	}
-}
